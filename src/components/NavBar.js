9560465593d9de30cd980009f5843d734fef8817
onam <onamkrverma@gmail.com>
2023-12-30 14:17:21 +0530

    fix: bg color of search suggestions

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,119 +1,151 @@
-import React, { useState, useContext, useRef, useEffect } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import MemesContext from '../context/MemesContext';
+import React, { useState, useContext, useRef, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import MemesContext from "../context/MemesContext";
 
 const NavBar = () => {
-  const [isMenuActive, setIsMenuActive] = useState(false)
+  const [isMenuActive, setIsMenuActive] = useState(false);
   const context = useContext(MemesContext);
-  const { isDarkMode, modeToggle, memesData } = context
-  const [searchTerm, setSearchTerm] = useState('')
+  const { isDarkMode, modeToggle, memesData } = context;
+  const [searchTerm, setSearchTerm] = useState("");
   const [searchTermFilter, setSearchTermFilter] = useState([]);
 
   let navigate = useNavigate();
   const inputRef = useRef(null);
 
   const handleSubmit = (e) => {
-    e.preventDefault()
-    const value = e.target.search.value
+    e.preventDefault();
+    const value = e.target.search.value;
     navigate(`/search/${value}`, {
-      state: { searchValue: value }
-    })
-
-  }
+      state: { searchValue: value },
+    });
+  };
 
   useEffect(() => {
-   
     const timer = setTimeout(() => {
       if (searchTerm) {
-        const dataFilter = memesData.map((data) => data.title).filter((val) => (val.toLowerCase()).includes(searchTerm.toLowerCase()));
-        setSearchTermFilter(dataFilter)
-      }
-      else{
-        setSearchTermFilter([])
+        const dataFilter = memesData
+          .map((data) => data.title)
+          .filter((val) =>
+            val.toLowerCase().includes(searchTerm.toLowerCase())
+          );
+        setSearchTermFilter(dataFilter);
+      } else {
+        setSearchTermFilter([]);
       }
     }, 1000);
-    return () => clearTimeout(timer)
-  }, [searchTerm])
+    return () => clearTimeout(timer);
+  }, [searchTerm]);
 
   const handleSearchTerm = (e) => {
     setSearchTerm(e.target.innerText);
     inputRef.current.focus();
-
-  }
-  
+  };
 
   // console.log(searchTerm)
   // console.log(searchTermFilter[0])
 
   const activeMenue = () => {
-    setIsMenuActive(!isMenuActive)
-  }
+    setIsMenuActive(!isMenuActive);
+  };
 
   const handleDarkMode = () => {
-    modeToggle()
-
-  }
+    modeToggle();
+  };
 
   return (
-    <nav >
-      <div className={`navBar ${isDarkMode ? 'darkModeActive' : ""} `}>
+    <nav>
+      <div className={`navBar ${isDarkMode ? "darkModeActive" : ""} `}>
         <div className="logo">
-          <img src="../image/mainIcon.png" width='40' height='40' alt="icon" />
-          <Link to="/"><h1>Okv-Memes</h1></Link>
+          <img src="../image/mainIcon.png" width="40" height="40" alt="icon" />
+          <Link to="/">
+            <h1>Okv-Memes</h1>
+          </Link>
         </div>
-        <div className="navLinks" style={{ display: isMenuActive ? "flex" : '' }}>
-          <ul >
-            <li><Link to="/" >Home</Link></li>
-
+        <div
+          className="navLinks"
+          style={{ display: isMenuActive ? "flex" : "" }}
+        >
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
           </ul>
         </div>
-        <div className="searchBox" style={{ display: isMenuActive ? "block" : '' }}>
+        <div
+          className="searchBox"
+          style={{ display: isMenuActive ? "block" : "" }}
+        >
           <form onSubmit={handleSubmit} autoComplete="off">
             <input
               type="text"
-              placeholder='Search...'
-              name='search'
-              autoComplete='off'
+              placeholder="Search..."
+              name="search"
+              autoComplete="off"
               onChange={(e) => setSearchTerm(e.target.value)}
               value={searchTerm}
               ref={inputRef}
-              required />
-            <span onClick={() => setSearchTerm('')} >&times;</span>
-            <button type='submit' className="submitBtn" style={{ background: isDarkMode ? '#ffffff14' : "" }} title='submit'>
+              required
+            />
+            <span onClick={() => setSearchTerm("")}>&times;</span>
+            <button
+              type="submit"
+              className="submitBtn"
+              style={{ background: isDarkMode ? "#ffffff14" : "" }}
+              title="submit"
+            >
               <i className="fa-solid fa-magnifying-glass"></i>
             </button>
           </form>
-          {(searchTerm && searchTerm?.trim() !== searchTermFilter[0]?.trim()) &&
-            <div className="search-auto-box" style={{height: !searchTermFilter.length && 'auto'}}>
-              { searchTermFilter?.map((title, index) =>
-                <div className="search-title" key={index} onClick={handleSearchTerm}>
+          {searchTerm && searchTerm?.trim() !== searchTermFilter[0]?.trim() && (
+            <div
+              className="search-auto-box"
+              style={{
+                height: !searchTermFilter.length && "auto",
+                backgroundColor: isDarkMode ? "#181818" : "",
+              }}
+            >
+              {searchTermFilter?.map((title, index) => (
+                <div
+                  className="search-title"
+                  key={index}
+                  onClick={handleSearchTerm}
+                >
                   {title}
-                </div>)
-                }
-                
-                
-              
+                </div>
+              ))}
             </div>
-
-          }
+          )}
         </div>
 
-
-        <button className="darkModeBtn" type='button' title='switch theme' style={{ display: isMenuActive ? "block" : '' }} onClick={handleDarkMode}>
-          <i className="fa-solid fa-moon" style={{ display: isDarkMode ? 'none' : 'block' }} ></i>
-          <i className="fa-solid fa-sun" style={{ display: isDarkMode ? 'block' : 'none' }}></i>
+        <button
+          className="darkModeBtn"
+          type="button"
+          title="switch theme"
+          style={{ display: isMenuActive ? "block" : "" }}
+          onClick={handleDarkMode}
+        >
+          <i
+            className="fa-solid fa-moon"
+            style={{ display: isDarkMode ? "none" : "block" }}
+          ></i>
+          <i
+            className="fa-solid fa-sun"
+            style={{ display: isDarkMode ? "block" : "none" }}
+          ></i>
           <p>{isDarkMode ? "Light Mode" : "Dark Mode"}</p>
         </button>
 
-        <div className={`menuBar ${isMenuActive ? "menuActive" : ""}`} onClick={activeMenue}>
+        <div
+          className={`menuBar ${isMenuActive ? "menuActive" : ""}`}
+          onClick={activeMenue}
+        >
           <div className="line line1"></div>
           <div className="line line2"></div>
           <div className="line line3"></div>
         </div>
       </div>
     </nav>
+  );
+};
 
-  )
-}
-
-export default NavBar
+export default NavBar;
