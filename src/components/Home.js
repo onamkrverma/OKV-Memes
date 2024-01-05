8f75e512432c63b60e207f314e7dbb41fb6c112e
onam <onamkrverma@gmail.com>
2024-01-05 15:57:57 +0530

    update hero section

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,36 +1,98 @@
-import React ,{useContext} from 'react';
-import Memes from './Memes';
-import MemesContext from '../context/MemesContext';
+import React, { useContext, useEffect, useRef, useState } from "react";
+import Memes from "./Memes";
+import MemesContext from "../context/MemesContext";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
   const context = useContext(MemesContext);
-  const {isDarkMode} = context;
+  const { isDarkMode, memesData } = context;
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchTermFilter, setSearchTermFilter] = useState([]);
+  const navigate = useNavigate();
+  const inputRef = useRef(null);
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const value = e.target.search.value;
+    navigate(`/search/${value}`, {
+      state: { searchValue: value },
+    });
+  };
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (searchTerm) {
+        const dataFilter = memesData
+          .map((data) => data.title)
+          .filter((val) =>
+            val.toLowerCase().includes(searchTerm.toLowerCase())
+          );
+        setSearchTermFilter(dataFilter);
+      } else {
+        setSearchTermFilter([]);
+      }
+    }, 1000);
+    return () => clearTimeout(timer);
+  }, [searchTerm]);
+
+  const handleSearchTerm = (e) => {
+    setSearchTerm(e.target.innerText);
+    inputRef.current.focus();
+  };
   return (
     <>
-
-      <div className='container'>
-        <div className={`heroSection ${isDarkMode?"darkModeActive":""}`}>
+      <div className="heroContainer">
+        <div className={`heroSection`}>
           <div className="heroContent">
-            <h1>Make Video More Engaging</h1>
-            <p>Adding memes to your video can increase video audience retention</p>
-            
+            <h1>Boost Your Video with Memes</h1>
+            <p>
+              Memes can enhance your video content and make it more engaging and
+              shareable.
+            </p>
           </div>
-          <div className="heroImage">
-            <img src="./image/poster.png" width='384' heigth='372' alt="poster img" style={{ display: 'block' }} />
+          <div className="searchBox ">
+            <form onSubmit={handleSubmit} autoComplete="off">
+              <input
+                type="text"
+                placeholder="Search..."
+                name="search"
+                autoComplete="off"
+                onChange={(e) => setSearchTerm(e.target.value)}
+                value={searchTerm}
+                ref={inputRef}
+                required
+              />
+              <span onClick={() => setSearchTerm("")}>&times;</span>
+              <button type="submit" className="submitBtn" title="submit">
+                <i className="fa-solid fa-magnifying-glass"></i>
+              </button>
+            </form>
+            {searchTerm &&
+              searchTerm?.trim() !== searchTermFilter[0]?.trim() && (
+                <div
+                  className="search-auto-box"
+                  style={{
+                    height: !searchTermFilter.length && "auto",
+                  }}
+                >
+                  {searchTermFilter?.map((title, index) => (
+                    <div
+                      className="search-title"
+                      key={index}
+                      onClick={handleSearchTerm}
+                    >
+                      {title}
+                    </div>
+                  ))}
+                </div>
+              )}
           </div>
-          <div className="circle"></div>
-
         </div>
-
       </div>
 
       <Memes />
-
-
-
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
